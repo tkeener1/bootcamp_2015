096a68db96085bdbe51e7399a2ea90dd683f9528
tkeener1 <troy.keener@us.sogeti.com>
2015-03-05 19:06:24 -0500

    Elements & Methods added

--- features/support/helper_methods.rb
+++ features/support/helper_methods.rb
@@ -32,6 +32,23 @@ module HelperMethods
       result
     end
 
+    def compare(compare_to,expected,actual)
+      case compare_to
+        when '', /equal/
+          expected == actual
+        when 'less than'
+          actual < expected
+        when 'greater than'
+          actual > expected
+        when 'or less'
+          actual <= expected
+        when 'or more', 'at least'
+          actual >= expected
+        else
+          fail ("Comparison #{compare_to} not supported")
+      end
+    end
+
   end
 
   # make Methods instance methods in objects that include HelperMethods
--- pages/usa_home_page.rb
+++ pages/usa_home_page.rb
@@ -7,4 +7,54 @@ class UsaHomePage
     @url = 'www.usa.gov'
   end
 
+
+  #define elements
+
+  def search
+   BROWSER.text_field(:id=>"query")
+  end
+
+   def submit
+    BROWSER.button(:id => "buscarSubmit")
+  end
+
+  def results
+    BROWSER.divs(:id => /^result-/)
+  end
+
+  def no_results_message
+    BROWSER.div(:id => 'no-results')
+  end
+
+ #Service Methods
+  def search_value
+    search.placeholder == '' ? search.value : search.placeholder
+  end
+
+  def submit_label
+    submit.text
+  end
+
+  def submit_search(query)
+    search.set query
+    submit.click
+  end
+
+  def results_count
+    results.size
+  end
+
+  def message
+    no_results_message.text
+  end
+
+
+
+
+
+
+
+
+
+
 end
